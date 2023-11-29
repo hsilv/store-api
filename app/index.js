9348810f33948c76479e0d3eda2601a0419711e2
hsilv <sil21764@uvg.edu.gt>
2023-11-29 13:31:12 -0600

    fix: (Products) Product routes import

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-const productsRouter = require('./products/routes/products.js');
+const productsRouter = require('./products/routes/products.routes.js');
 
 function useProducts(app) {
   app.use('/products', productsRouter);
